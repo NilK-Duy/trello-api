0daf2dd6f0786329579ba3b3cdc37e02a59d1135
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-04-08 16:09:48 +0700

    #13 get api details of board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCode } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   try {
@@ -26,6 +28,19 @@ const createNew = async (reqBody) => {
   } catch (error) { throw error }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    // Gọi tới tầng Model để xử lý lưu bản ghi getDetails vào Database
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCode.NOT_FOUND, 'Board not found!')
+    }
+    // Trả kết quả về, trong Service luôn phải có return
+    return board
+  } catch (error) { throw error }
+}
+
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
