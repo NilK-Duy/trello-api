a5dd47da8b13e3b63f9f48cc319f12be33fb1df1
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-04-08 17:05:34 +0700

    #14 config cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-/**
-
- */
+// Những domain được phép truy cập tới tài nguyên chính của Server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // ... deploy lên domain chính thức
+]
