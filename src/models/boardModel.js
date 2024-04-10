ad3259772c1ee6a8dc1fbbe88c40a28d4fdcbb9a
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-04-10 14:00:08 +0700

    #20 move cards v2 & fix bug

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -101,6 +101,11 @@ const update = async (boardId, updateData) => {
       }
     })
 
+    // Biến đổi những dữ liệu liên quan ObjectId
+    if (updateData.columnOrderIds) {
+      updateData.columnOrderIds = updateData.columnOrderIds.map(_id => new ObjectId(_id))
+    }
+
     const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
       { _id: new ObjectId(boardId) },
       { $set: updateData },
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
 import { cloneDeep } from 'lodash'
@@ -43,10 +45,10 @@ const getDetails = async (boardId) => {
     // B2: Đưa card về đúng column của nó
     resBoard.columns.forEach(column => {
       // Cách dùng .equals() cho thấy ObjectId trong MongoDB có support method .equals()
-      // column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
 
       // // Cách này convert ObjectId về string bằng hàm toString() của JavaScript
-      column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+      // column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
     })
 
     // B3: Xóa mảng cards khỏi board ban đầu
@@ -69,8 +71,30 @@ const update = async (boardId, reqBody) => {
   } catch (error) { throw error }
 }
 
+const moveCardToDifferentColumn = async (reqBody) => {
+  try {
+    // B1: Cập nhật mảng cardOrderIds của Column ban đầu chứa nó (xóa _id của card ra khỏi mảng)
+    await columnModel.update(reqBody.prevColumnId, {
+      cardOrderIds: reqBody.prevCardOrderIds,
+      updatedAt: Date.now()
+    })
+    // B2: Cập nhật mảng cardOrderIds của Column tiếp theo (thêm _id của card vào mảng)
+    await columnModel.update(reqBody.nextColumnId, {
+      cardOrderIds: reqBody.nextCardOrderIds,
+      updatedAt: Date.now()
+    })
+    // B3: Cập nhật lại trường columnId mới của card đã kéo
+    await cardModel.update(reqBody.currentCardId, {
+      columnId: reqBody.nextColumnId
+    })
+
+    return { updateResult: 'Successfull!' }
+  } catch (error) { throw error }
+}
+
 export const boardService = {
   createNew,
   getDetails,
-  update
+  update,
+  moveCardToDifferentColumn
 }
