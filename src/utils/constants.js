b3e74d786eaca0a308cf8c59399a66c0519a34d2
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-04-10 23:41:42 +0700

    ADD NEW DOMAIN

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,8 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // vì ở file config/cors đã luôn luôn cho phép môi trường dev
   // ... deploy lên domain chính thức
-  'https://trello-web-eight-nu.vercel.app'
+  'https://trello-web-eight-nu.vercel.app',
+  'https://ngkhanhduy.id.vn'
 ]
 
 export const BOARD_TYPES = {
